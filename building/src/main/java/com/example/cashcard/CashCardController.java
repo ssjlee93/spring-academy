61231c413a2d7b192799a89a3c0bb7293a535d50
Steve S. Lee <sjlee12@me.com>
2025-02-13 00:07:23 -0500

    finished 3rd lab

---
+++ building/src/main/java/com/example/cashcard/CashCardController.java
@@ -0,0 +1,24 @@
+package com.example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// component of type RestController
+@RestController
+// indicates which address request must have to access this controller
+@RequestMapping("/cashcards")
+public class CashCardController {
+
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if (requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
